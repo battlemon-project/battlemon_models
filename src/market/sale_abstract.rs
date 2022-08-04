68db923b88c386bc8ba07de7235f65c82e98fd9a
ChzenChzen <fedorovdanila@me.com>
2022-08-04 22:16:09 +0300

    delete redundant

--- src/market/sale_abstract.rs
+++
@@ -1,7 +0,0 @@
-#[mixin::declare]
-#[derive(Debug, Clone, PartialEq)]
-pub struct SaleAbstract {
-    pub prev_owner: String,
-    pub curr_owner: String,
-    pub token_id: String,
-}
--- src/market/sale_contract.rs
+++
@@ -1,8 +0,0 @@
-use near_sdk::json_types::U128;
-
-#[mixin::insert(SaleAbstract)]
-#[derive(near_sdk::serde::Serialize, near_sdk::serde::Deserialize, Debug, Clone)]
-#[serde(crate = "near_sdk::serde")]
-pub struct SaleForContract {
-    pub price: U128,
-}
