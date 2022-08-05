7cc99ec09bcbed9cc5a2dab8580c93c3dd28f75f
ChzenChzen <fedorovdanila@me.com>
2022-08-05 13:53:02 +0300

    fix convertion time

--- src/market.rs
+++ src/market.rs
@@ -1,3 +1,5 @@
+use chrono::Duration;
+
 pub mod ask;
 pub mod bid;
 pub mod events;
@@ -15,7 +17,7 @@ fn convert_to_decimal_near(yocto_near: &near_sdk::json_types::U128) -> rust_deci
 #[cfg(feature = "market-convert")]
 fn convert_from_near_timestamp(timestamp: u64) -> chrono::DateTime<chrono::Utc> {
     use chrono::{DateTime, NaiveDateTime, Utc};
-    let timestamp = timestamp.try_into().unwrap();
-    let naive = NaiveDateTime::from_timestamp(0, timestamp);
+    let duration = Duration::nanoseconds(timestamp);
+    let naive = NaiveDateTime::from_timestamp(duration.num_seconds(), 0);
     DateTime::<Utc>::from_utc(naive, Utc)
 }
