119c9bf72b8a12044d29fce81e230c7286bce38d
ChzenChzen <fedorovdanila@me.com>
2022-09-25 16:00:13 +0300

    refactor events in models

--- src/nft/events.rs
+++ src/nft/events.rs
@@ -1,9 +1,43 @@
-use crate::nft::token::TokenExt;
+#[derive(Debug, Clone, near_sdk::serde::Serialize, near_sdk::serde::Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+#[serde(rename_all = "snake_case")]
+pub enum NftEventKind {
+    NftMint,
+    NftBurn,
+    AssembleNft,
+    DisassembleNft,
+}
 
 #[derive(Debug, Clone, near_sdk::serde::Serialize, near_sdk::serde::Deserialize)]
 #[serde(crate = "near_sdk::serde")]
-#[serde(rename_all = "snake_case", tag = "event", content = "data")]
-pub enum NftEventKind {
-    AssembleNft(TokenExt),
-    DisassembleNft(TokenExt),
+pub enum StandardKind {
+    #[serde(rename = "nep171")]
+    Nep171,
+}
+
+#[derive(Debug, Clone, near_sdk::serde::Serialize, near_sdk::serde::Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum VersionKind {
+    #[serde(rename = "1.0.0")]
+    V1_0_0,
+}
+
+#[derive(Debug, Clone, near_sdk::serde::Serialize, near_sdk::serde::Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+#[serde(untagged)]
+pub enum NftEventLogKind {
+    NftMintLog {
+        owner_id: String,
+        token_ids: Vec<String>,
+        memo: Option<String>,
+    },
+}
+
+#[derive(Debug, Clone, near_sdk::serde::Serialize, near_sdk::serde::Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct NftEvent {
+    pub standard: StandardKind,
+    pub version: VersionKind,
+    pub event: NftEventKind,
+    pub data: Option<Vec<NftEventLogKind>>,
 }
